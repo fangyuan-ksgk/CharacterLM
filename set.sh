69687a7bc3fd8d8b2883b7acc9433d9eebd48a06
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-01-24 00:33:09 +0800

    update train script to use accelerate package

--- set.sh
+++ set.sh
@@ -1,6 +1,8 @@
 api install zip
 pip install torch numpy transformers datasets tiktoken wandb tqdm
+pip install accelerate
 python data/enwiki/prepare_data.py
 
 wandb login
+accelerate config
 # python train.py config/train_enwiki_char.py
