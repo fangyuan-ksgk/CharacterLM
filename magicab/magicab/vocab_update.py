38b6f8032b409a2aad8632733f48f101097cc55f
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-02-12 14:05:40 +0800

    update fix for sorting token_removal gadget

--- magicab/magicab/vocab_update.py
+++ magicab/magicab/vocab_update.py
@@ -281,9 +281,12 @@ def add_to_vocab(self, max_size_change: int = 500):
     
 @timing_decorator
 def remove_from_vocab(self, max_size_change: int = 500): 
-    print(":: Removing from vocabulary ...")
-    tokens_to_remove = sorted(self.token_removal.keys(), key=lambda x: self.token_removal[x], reverse=True)[:max_size_change]
-    print(f":: Total {len(tokens_to_remove)} tokens to remove")
+    print(f":: Removing from vocabulary, selecting {max_size_change} from {len(self.token_removal)} candidates ...")
+    
+    tokens_to_remove = list(self.token_removal.keys())[:max_size_change]
+    # tokens_to_remove = sorted(self.token_removal.keys(), key=lambda x: self.token_removal[x], reverse=True)[:max_size_change]
+    
+    print(f":: Picked total {len(tokens_to_remove)} tokens to remove")
     self.tokenizer.remove_tokens(tokens_to_remove)
     new_wte = remove_token_wte(self.model.transformer.wte, tokens_to_remove)
     new_lm_head = remove_token_lm_head(self.model.lm_head, tokens_to_remove)
