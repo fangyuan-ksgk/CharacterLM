58cc3294b18bf71c9d68eaa0edfaef7452baa125
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-02-13 12:15:43 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
-python data/enwiki/prepare_data.py --clean
 run_dir="checkpoint/run1"
 mkdir -p $run_dir
 
-python train.py config/train_enwiki_char.py --out_dir="$run_dir"
-python update.py --out_dir="$run_dir" --new_dir="$run_dir/iter1_raw" --thres=0.3 --max_size_change=3000
+python data/enwiki/prepare_data.py --clean --out_dir="$run_dir/base"
+
+python train.py config/train_enwiki_char.py --out_dir="$run_dir/base"
+python update.py --out_dir="$run_dir/base" --new_dir="$run_dir/iter1_raw" --thres=0.3 --max_size_change=3000
 mkdir -p $run_dir/iter1
 cp -r $run_dir/iter1_raw/* $run_dir/iter1
 
