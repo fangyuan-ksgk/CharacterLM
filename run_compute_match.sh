198755adf69d38a438ba979de99c9b2f458a0374
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-02 00:20:08 +0800

    add fix for compute matching script

--- run_compute_match.sh
+++ run_compute_match.sh
@@ -1,6 +1,7 @@
 # Compute Matching Experiment (v.s. run5)
 orig_run_dir="checkpoint/gpt_small"
 run_dir="checkpoint/gpt_small_cm"
+data_subfolder="gpt_small"
 mkdir -p $run_dir
 
 num_iterations=8  # Adjust this number as needed
@@ -13,16 +14,17 @@ for iter in $(seq 1 $num_iterations); do
     curr_dir="${run_dir}/increase_iter${iter}"
     
     # prepare encoding data 
-    python data/enwiki/prepare_data.py --clean --out_dir="${curr_dir}" --tokenizer_path="${orig_dir}/tokenizer.json"
+    python data/enwiki/prepare_data.py --clean --out_dir="${curr_dir}" --tokenizer_path="${orig_dir}/tokenizer.json" --data_subfolder="${data_subfolder}"
     
     # Train and evaluate
-    python train.py config/train_enwiki_gpt_small.py --init_from="retrain" --load_dir="${orig_dir}" --out_dir="${curr_dir}" --max_iters=${accumulated_iter} --data_subfolder=""
+    python train.py config/train_enwiki_gpt_small.py --init_from="retrain" --load_dir="${orig_dir}" --out_dir="${curr_dir}" --max_iters=${accumulated_iter} --data_subfolder="${data_subfolder}"
     python eval.py --model_type="GPT" --out_dir="${run_dir}/increase_iter${iter}" --run_idx=${iter}
 
 done
 
 orig_run_dir="checkpoint/gpt_medium"
 run_dir="checkpoint/gpt_medium_cm"
+data_subfolder="gpt_medium"
 mkdir -p $run_dir
 
 num_iterations=8  # Adjust this number as needed
@@ -35,10 +37,10 @@ for iter in $(seq 1 $num_iterations); do
     curr_dir="${run_dir}/increase_iter${iter}"
     
     # prepare encoding data 
-    python data/enwiki/prepare_data.py --clean --out_dir="${curr_dir}" --tokenizer_path="${orig_dir}/tokenizer.json"
+    python data/enwiki/prepare_data.py --clean --out_dir="${curr_dir}" --tokenizer_path="${orig_dir}/tokenizer.json" --data_subfolder="${data_subfolder}"
     
     # Train and evaluate
-    python train.py config/train_enwiki_gpt_medium.py --init_from="retrain" --load_dir="${orig_dir}" --out_dir="${curr_dir}" --max_iters=${accumulated_iter} --data_subfolder=""
+    python train.py config/train_enwiki_gpt_medium.py --init_from="retrain" --load_dir="${orig_dir}" --out_dir="${curr_dir}" --max_iters=${accumulated_iter} --data_subfolder="${data_subfolder}"
     python eval.py --model_type="GPT" --out_dir="${run_dir}/increase_iter${iter}" --run_idx=${iter}
 
 done
