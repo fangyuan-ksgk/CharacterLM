7ad88ab6982fd904c350f4c655cc2cb4f0495f09
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-02 14:44:32 +0800

    update data_subfolder default value

--- run_base.sh
+++ run_base.sh
@@ -1,4 +1,6 @@
 run_dir="checkpoint/base"
-python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
+python train.py config/train_enwiki_char.py --out_dir="$run_dir"
+
+# python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
 python train.py config/train_enwiki_char.py --out_dir="$run_dir"
 python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
--- train.py
+++ train.py
@@ -49,7 +49,7 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
-data_subfolder="enwiki"
+data_subfolder=""
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
 batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
