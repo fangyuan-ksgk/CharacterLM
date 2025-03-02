39a39a5aaad129cb7b042a9767c40450ed98c8e5
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-02 14:38:43 +0800

    update base script

--- run_base.sh
+++ run_base.sh
@@ -1,4 +1,4 @@
 run_dir="checkpoint/base"
 python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
-python train.py config/train_enwiki_char.py --out_dir="$run_dir"
+python train.py config/train_enwiki_char.py --out_dir="$run_dir" --max_iters=5000
 python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
