c4ad1da36d472483e79ebf14562c4d5399367d8d
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-02 14:30:40 +0800

    update script

--- run_base.sh
+++ run_base.sh
@@ -1,13 +1,4 @@
-run_dir="checkpoint/run4"
-python data/enwiki/prepare_data.py --clean --out_dir="$run_dir/max_vocab" --tokenizer_path="checkpoint/run3/increase_iter5/tokenizer.json"
-python train.py config/train_enwiki_char.py --out_dir="$run_dir/max_vocab" --max_iters=30000
-python eval.py --model_type="GPT" --out_dir="$run_dir/max_vocab" --run_idx=0
-
-python data/enwiki/prepare_data.py --clean --out_dir="$run_dir/min_vocab" --tokenizer_path="checkpoint/run3/base/tokenizer.json"
-python train.py config/train_enwiki_char.py --out_dir="$run_dir/min_vocab" --max_iters=30000
-python eval.py --model_type="GPT" --out_dir="$run_dir/min_vocab" --run_idx=0
-
-
-
-
-python train_sft.py config/train_sft_gpt.py --load_dir="checkpoint/base" --out_dir="checkpoint/sft_gpt" --max_iters=5000 --device="mps"
+run_dir="checkpoint/base"
+python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
+python train.py config/train_enwiki_char.py --out_dir="$run_dir"
+python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
