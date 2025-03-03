21609277bc825931924702d44a6ada1a9b1ed413
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-03 23:22:34 +0800

    update code, quick check on data encoding with vocab update

--- run_vocab_curriculum.sh
+++ run_vocab_curriculum.sh
@@ -7,7 +7,7 @@ python data/enwiki/prepare_data.py --clean --out_dir="$run_dir/base" --data_subf
 python train.py $config_file --out_dir="$run_dir/base" --data_subfolder="$data_subfolder"
 python eval.py --model_type="GPT" --out_dir="$run_dir/base" --run_idx=0 --data_subfolder="$data_subfolder"
 
-num_iterations=6  
+num_iterations=6 1
 for iter in $(seq 1 $num_iterations); do
     prev_iter=$((iter - 1))
     prev_dir="$run_dir/$([ $prev_iter -eq 0 ] && echo 'base' || echo "increase_iter${prev_iter}")"
@@ -16,13 +16,13 @@ for iter in $(seq 1 $num_iterations); do
     python update.py --out_dir="$prev_dir" --new_dir="$run_dir/increase_iter${iter}_raw" --data_subfolder="$data_subfolder" \
         --thres=0.3 --max_size_change=3000
     
-    # Create new directory and copy files
-    mkdir -p "$run_dir/increase_iter${iter}"
-    cp -r "$run_dir/increase_iter${iter}_raw"/* "$run_dir/increase_iter${iter}"
+    # # Create new directory and copy files
+    # mkdir -p "$run_dir/increase_iter${iter}"
+    # cp -r "$run_dir/increase_iter${iter}_raw"/* "$run_dir/increase_iter${iter}"
     
-    # Train and evaluate
-    python train.py $config_file --init_from="resume" --out_dir="$run_dir/increase_iter${iter}" --data_subfolder="$data_subfolder"
-    python eval.py --model_type="GPT" --out_dir="$run_dir/increase_iter${iter}" --run_idx=$iter --data_subfolder="$data_subfolder"
+    # # Train and evaluate
+    # python train.py $config_file --init_from="resume" --out_dir="$run_dir/increase_iter${iter}" --data_subfolder="$data_subfolder"
+    # python eval.py --model_type="GPT" --out_dir="$run_dir/increase_iter${iter}" --run_idx=$iter --data_subfolder="$data_subfolder"
 done
 
 
