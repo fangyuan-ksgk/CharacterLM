07ec0a41bb471f7bedd3697e3af2e0af6f55c89f
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-07 11:41:34 +0800

    update script for process pt dataset

--- data/composio/proc_data.sh
+++ data/composio/proc_data.sh
@@ -1,3 +1,3 @@
 python data/composio/download_pt_data.py --save_dir="data/composio/datasets" --cache_dir="data/composio/cache" --total_chars=2000000000
 
-python data/composio/process_pt_data.py --datasets_dir="data/composio/datasets" --cache_dir="data/composio/cache" --total_chars=2000000000
+python data/composio/process_pt_data.py --datasets_dir="data/composio/datasets" --save_dir="data/composio/processed" --tokenizer_name_or_path="checkpoint/base/tokenizer.json" --block_size=512
