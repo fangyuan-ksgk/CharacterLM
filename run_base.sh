329bff8843a81a95eca53edb63ef56d6abddbdf9
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-11 11:59:02 +0800

    update byte train script

--- run_base.sh
+++ run_base.sh
@@ -4,15 +4,20 @@
 # python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
 
 
-run_name = "base_byte"
-run_dir="checkpoint/base_byte"
+run_name="base_byte"
+run_dir="checkpoint/$run_name"
+
+# Create directory if it doesn't exist
+mkdir -p "$run_dir"
+
 python data/composio/process_pt_data.py --datasets_dir="data/composio/datasets"\
                                         --save_dir="data/composio/$run_name"\
-                                        --tokenizer_name_or_path="checkpoint/$run_name"\
+                                        --tokenizer_name_or_path="$run_dir"\
                                         --block_size=512\
                                         --mode="byte"\
                                         --init_vocab=True\
                                         --batch_size=20\
                                         --max_workers=8
-python train.py config/train_smol_byte.py --data_subfolder="$run_name" --out_dir="checkpoint/smol_byte/base_byte"
-python eval.py --model_type="GPT" --data_subfolder="$run_name" --out_dir="checkpoint/smol_byte/base_byte" --run_idx=0
+
+python train.py config/train_smol_byte.py --data_subfolder="$run_name" --out_dir="$run_dir"
+python eval.py --model_type="GPT" --data_subfolder="$run_name" --out_dir="$run_dir" --run_idx=0
