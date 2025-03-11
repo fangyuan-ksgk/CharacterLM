d16c75c327c286779b934b66b3b06702587ba517
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-11 11:56:38 +0800

    update train script for byte tokenizer & smol dataset

---
+++ config/train_smol_byte.py
@@ -0,0 +1,43 @@
+# train a miniature character-level enwiki9 model
+# good for debugging and playing on macbooks and such
+
+# load_dir = 'checkpoint/smol_byte/gpt_tiny'
+init_from = 'scratch'
+mode = 'byte'
+out_dir = 'checkpoint/smol_byte/gpt_tiny'
+eval_interval = 250 # keep frequent because we'll overfit
+eval_iters = 200
+log_interval = 10 # don't print too too often
+
+# we expect to overfit on this small dataset, so only save when val improves
+always_save_checkpoint = False
+
+wandb_log = False # override via command line if you like
+wandb_project = 'smol_byte'
+wandb_run_name = 'gpt_tiny'
+
+dataset = 'composio/datasets'
+data_subfolder="gpt_tiny"
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 512 # context of up to 512 previous characters
+
+# baby GPT model :) | GPT-2 Small Scale Model 
+model_type = "GPT"
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 5e-4 # gpt small 124M learning rate
+max_iters = 5000
+magicab_interval = 1000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
--- run_base.sh
+++ run_base.sh
@@ -1,10 +1,18 @@
-run_dir="checkpoint/base"
-python train.py config/train_enwiki_char.py --out_dir="$run_dir"
-
+# run_dir="checkpoint/base"
 # python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
-python train.py config/train_enwiki_char.py --out_dir="$run_dir"
-python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
+# python train.py config/train_enwiki_char.py --out_dir="$run_dir"
+# python eval.py --model_type="GPT" --out_dir="$run_dir" --run_idx=0
 
 
-
-python data/enwiki/prepare_data.py --clean --out_dir="$run_dir"
+run_name = "base_byte"
+run_dir="checkpoint/base_byte"
+python data/composio/process_pt_data.py --datasets_dir="data/composio/datasets"\
+                                        --save_dir="data/composio/$run_name"\
+                                        --tokenizer_name_or_path="checkpoint/$run_name"\
+                                        --block_size=512\
+                                        --mode="byte"\
+                                        --init_vocab=True\
+                                        --batch_size=20\
+                                        --max_workers=8
+python train.py config/train_smol_byte.py --data_subfolder="$run_name" --out_dir="checkpoint/smol_byte/base_byte"
+python eval.py --model_type="GPT" --data_subfolder="$run_name" --out_dir="checkpoint/smol_byte/base_byte" --run_idx=0
