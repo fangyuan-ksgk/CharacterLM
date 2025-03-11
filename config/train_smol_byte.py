fb1148e5d95cec5b4740e05ac98b657e4e5fc1c3
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-11 14:01:39 +0800

    increase iter count for smolLM training

--- config/train_smol_byte.py
+++ config/train_smol_byte.py
@@ -12,7 +12,7 @@ log_interval = 10 # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
+wandb_log = True # override via command line if you like
 wandb_project = 'smol_byte'
 wandb_run_name = 'gpt_tiny'
 
@@ -30,7 +30,7 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 5e-4 # gpt small 124M learning rate
-max_iters = 5000
+max_iters = 50000
 magicab_interval = 1000
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
