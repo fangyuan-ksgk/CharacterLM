d79eea7e9a040cfab0a0c84c8cec7407280a8626
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2025-03-25 11:10:24 +0800

    update config file for openweb gpt training runs

--- data/composio/process_pt_data.py
+++ data/composio/process_pt_data.py
@@ -3,15 +3,15 @@ from util import process_composio_pt_data
 
 def main(args):
     
-    print("Loaded tokenizer from:  ***", args.tokenizer_name_or_path,"***")
-    args.tokenizer_name_or_path = args.tokenizer_name_or_path.strip()
+    print("Loaded tokenizer from:  ***", args.tokenizer_path,"***")
+    args.tokenizer_path = args.tokenizer_path.strip()
     args.datasets_dir = args.datasets_dir.strip()
     args.save_dir = args.save_dir.strip()
     
     process_composio_pt_data(
         datasets_dir=args.datasets_dir,
         save_dir=args.save_dir,
-        tokenizer_path=args.tokenizer_name_or_path,
+        tokenizer_path=args.tokenizer_path,
         mode=args.mode,
         tokenizer=None,
         init_vocab=args.init_vocab,
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     argparser = ArgumentParser()
     argparser.add_argument("--datasets_dir", type=str, default="./datasets")
     argparser.add_argument("--save_dir", type=str, default="./datasets")
-    argparser.add_argument("--tokenizer_name_or_path", type=str, default="checkpoint/base")
+    argparser.add_argument("--tokenizer_path", type=str, default="checkpoint/base")
     argparser.add_argument("--mode", type=str, default="char")
     argparser.add_argument("--init_vocab", type=bool, default=False)
     argparser.add_argument("--num_proc", type=int, default=1)
--- run_base.sh
+++ run_base.sh
@@ -6,17 +6,18 @@
 
 run_name="base_byte"
 run_dir="checkpoint/$run_name"
-dataset_name="composio"
+# dataset_name="composio"
+dataset_name="openweb"
 
 # Create directory if it doesn't exist
 mkdir -p "$run_dir"
 
 python data/$dataset_name/process_pt_data.py --datasets_dir="data/$dataset_name/datasets"\
                                         --save_dir="data/$dataset_name/$run_name"\
-                                        --tokenizer_name_or_path="$run_dir"\ 
+                                        --tokenizer_path="$run_dir"\ 
                                         --mode="byte"\
                                         --init_vocab=True\
                                         --max_workers=8
 
 python train.py config/train_smol_byte.py --dataset="$dataset_name" --data_subfolder="$run_name" --out_dir="$run_dir"
-python eval.py --model_type="GPT" --dataset="$dataset_name" --data_subfolder="$run_name" --out_dir="$run_dir" --run_idx=1
+python eval.py --model_type="GPT" --dataset="$dataset_name" --data_subfolder="$run_name" --out_dir="$run_dir" --run_idx=0
